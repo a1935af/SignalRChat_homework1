b9e8d3c4e1d92c96092688d4a7122816ed80887e
a1935af <a1935af@aiit.ac.jp>
2020-03-09 21:22:45 +0900

    プロジェクト ファイルを追加します。

---
+++ SignalRChat/wwwroot/js/chat.js
@@ -0,0 +1,30 @@
+﻿"use strict";
+var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+
+connection.on("ReceiveMessage", function (id) {
+    if (document.getElementById(id).value == "可能") {
+        document.getElementById(id).value = "不可";
+        document.getElementById(id).className = "btn-danger";
+
+    } else {
+        document.getElementById(id).value = "可能";
+        document.getElementById(id).className = "btn-success";
+    };
+    document.getElementById(id).disabled = false;
+});
+
+connection.start().then(function () {
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+document.body.addEventListener("click", function (event) {
+    var id = event.target.id;
+    document.getElementById(id).disabled = true;
+    connection.invoke("SendMessage", id).catch(function (err) {
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+});
+
+
